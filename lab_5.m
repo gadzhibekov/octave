955d2a54ab5c29ee91d8c89bee3994102ff29d73
Beibala <ggbeibala05@gmail.con>
2025-04-09 23:09:31 +0500

    fixed lab_5

--- lab_5.m
+++ lab_5.m
@@ -25,14 +25,15 @@ xlabel('x');
 ylabel('y');
 title('Многочлены наилучшего среднеквадратичного приближения');
 legend show;
+grid on;
 
 
 % 1.4
-[~, idx] = min(sigma);
+[value, idx] = min(sigma);
 r = idx - 1;
 coeffs_r = polyfit(x, y, r);
 
-fprintf('Найденный многочлен степени %d:\n', r);
+fprintf('Найденный многочлен степени %d и значением %d:\n', r, value);
 fprintf('P_%d(x) = ', r);
 fprintf('%g', coeffs_r(1));
 for i = 2:length(coeffs_r)
@@ -42,29 +43,32 @@ fprintf('\n');
 
 
 % 2.1 - 2.2
-c = abs(min(y)) + 1;
-Y = log(y + c); 
-X = log(x);
+x = [0.5, 0.75, 1, 1.25, 1.5, 1.75, 2, 2.25, 2.5, 2.75, 3];
+y = [1.7499, 2.5732, 3.2817, 3.8197, 4.1396, 4.2065, 3.5208, 2.7829, 1.8224, 0.6915, 0.6915];
 
+
+Y = y ./ sin(x);
+X = x;
 
 % 2.3
 coefficients = polyfit(X, Y, 1);
 a = coefficients(1);
 b = coefficients(2);
 
-
 % 2.4
 figure;
-plot(X, Y, 'ko', 'MarkerSize', 8, 'MarkerFaceColor', 'r', 'DisplayName', 'Преобразованные данные');
+plot(X, Y, 'ro', 'MarkerFaceColor', 'r', 'DisplayName', 'Преобразованные данные');
 hold on;
+
 Y_fitted = a * X + b;
 plot(X, Y_fitted, 'g-', 'LineWidth', 2, 'DisplayName', 'Линейная зависимость');
-xlabel('ln(X)');
-ylabel('ln(Y)');
+
+% Оформление графика
+xlabel('X');
+ylabel('Y');
 title('Линейная зависимость после преобразования');
 legend show;
 grid on;
 
-
 % 2.5
-fprintf('Линейная зависимость имеет вид: Y = %.4f * X + %.4f\n', a, b);
+fprintf('Найденная линейная зависимость: Y = %.4f * X + %.4f\n', a, b);
